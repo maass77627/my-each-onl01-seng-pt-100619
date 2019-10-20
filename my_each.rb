7584d89349ab1a3ae619f447c51c9d28812c20c0
kelly r maass <kmaass77627@gmail.com>
2019-10-20 17:20:35 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,11 +1,12 @@
 def my_each(array)
   i = 0 
-  while i < array.length do |i|
-    yield array[i]
+  while i < array.length
+    yield(array[i])
     i = i + 1
+end
 end
 array = [1, 2, 3, 4]
 my_each(array) do |i|
   puts i 
-end 
+ end
 
